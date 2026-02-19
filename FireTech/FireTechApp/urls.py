76b589304684a21018510a597d584a5896fb2103
YSA_TheCrow <yael63008@gmail.com>
2026-02-19 16:53:12 -0500

    Funcion Crear_Mobile

--- FireTech/FireTechApp/urls.py
+++ FireTech/FireTechApp/urls.py
@@ -2,6 +2,9 @@ from FireTechApp import views
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Crear_mobiles/', views.Crear_mobile, name='Crear_mobiles'),
 ]
--- FireTech/FireTechApp/views.py
+++ FireTech/FireTechApp/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from FireTech.firebase_conex import initialize_firebase
+from django.contrib import messages
+
+
+
 
 # Create your views here.
 
 db = initialize_firebase()
 def index(request):
     return render(request, 'index.html')
+
+def Crear_mobile(request):
+    if request.method == 'POST':
+        
+        nombre = request.POST['nombre']
+        marca = request.POST['marca']
+        modelo = request.POST['modelo']
+        color = request.POST['color']
+        
+        try:
+            
+            db.collection('mobiles').add({
+                'nombre': nombre,
+                'marca': marca,
+                'modelo': modelo,
+                'color': color,
+
+            })
+            messages.success(request, '|✅| Mobile creado exitosamente')
+            return redirect('Listar_mobiles')
+        except Exception as e:
+            messages.error(request, f" |❌| Error al crear el mobile: {e}")
+            return render(request, 'Mobiles/Crear_mobiles.html', {'message': 'Error al crear el mobile'})
+    return render(request, 'Mobiles/Crear_mobiles.html')
