a20db8848714b2f75e69c5d4a83bfb599df45dba
Deivi1026 <davidlozanobedoya26@gmail.com>
2026-02-20 09:28:05 -0500

    Funcion de leer mobiles hecha

--- FireTech/FireTechApp/urls.py
+++ FireTech/FireTechApp/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('Listar_mobiles/', views.Listar_mobiles, name='Listar_mobiles'),
 ]
--- FireTech/FireTechApp/views.py
+++ FireTech/FireTechApp/views.py
@@ -1,3 +1,5 @@
+from pyexpat.errors import messages
+
 from django.shortcuts import render
 from FireTech.firebase_conex import initialize_firebase
 
@@ -6,3 +8,24 @@ from FireTech.firebase_conex import initialize_firebase
 db = initialize_firebase()
 def index(request):
     return render(request, 'index.html')
+
+
+def Listar_mobiles(request):
+    """
+    READ: recuperar las tareas dle usuario desde firebase 
+
+    """
+    
+    
+    mobiless = []
+    try:
+        #vamos a filtrar las tareas por el UID del usuario
+        docs = db.collection('mobiles').stream()
+        for doc in docs:
+            mobiles = doc.to_dict()
+            mobiles['id'] = doc.id  # Agregar el ID del documento a los datos del móvil
+            mobiless.append(mobiles)
+            print(mobiles)
+    except Exception as e:
+        messages.error(request, f"Error al cargar los móviles: {e}")
+    return render(request, 'Listar_mobiles.html', {'mobiles': mobiless})
